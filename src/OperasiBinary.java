4ca5e2783dddb6cfc07ef1a0db13dddd9fbce0f1
muhammadsyarifuddin23 <beldadina987@gmail.com>
2020-04-16 00:36:09 +0800

    OperasiBinary

---
+++ src/OperasiBinary.java
@@ -0,0 +1,14 @@
+public class OperasiBinary {
+    string nama = "MAHDINA";
+    int jumlahSks = 24;
+    double indeksPrestaiKumulatif = 3.89;
+
+    public  static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
