d860cb235626c81a20d55187df9b47655c2533aa
muhammadsyarifuddin23 <beldadina987@gmail.com>
2020-04-16 00:45:32 +0800

    OperasiBinary

---
+++ src/OperasiEqualiydanRelational.java
@@ -0,0 +1,15 @@
+public class OperasiEqualiydanRelational {
+    public static void main(string[] args) {
+        int i = 5;
+        int j = 6;
+        System.out.println(i==5);
+        System.out.println(i==6);
+        System.out.println(i==j);
+        System.out.println(i>j);
+        System.out.println(i<j);
+        System.out.println(5>6);
+        System.out.println(5<6);
+        System.out.println(i!=5);
+        System.out.println(j!=5);
+    }
+}
