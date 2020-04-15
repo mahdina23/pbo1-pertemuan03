b62070fee32cfe5730ff2646d23ecd30fe2e68a7
muhammadsyarifuddin23 <beldadina987@gmail.com>
2020-04-16 00:50:53 +0800

    percabanganifelse

---
+++ src/percabanganifelse.java
@@ -0,0 +1,16 @@
+public class percabanganifelse {
+
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDidompet = 25000;
+
+        if(uangDiDompet<totalBelanja) {
+            system.out.println("Uang kurang, kurangi jajan anda");
+        }elsa if (uangDidompet>totalBelanja) {
+            double angsul = uangDidompet - totalBelanja;
+            System.uot.println("Uang cukup, angsul : " + angsul);
+        }else{
+            System.out.println("Uang pas.. UwU..");
+        }
+    }
+}
