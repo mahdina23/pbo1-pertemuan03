d78bc10696b4b3c5b6f25ef0ed0f1566fd6e50ee
muhammadsyarifuddin23 <beldadina987@gmail.com>
2020-04-16 00:53:12 +0800

    Percabanganswitch

---
+++ src/Percabanganswitch.java
@@ -0,0 +1,22 @@
+public class Percabanganswitch {
+    public static void main(String[] args);
+    int jalanjalan=3;
+    switch(jalanjalan){
+        case 1:
+            System.out.println("urang lapas");
+            break;
+        case 2:
+            System.out.println("urang acayan");
+            break;
+        case 3:
+            System.out.println("ubat nyamuk");
+            break;
+        case 4:
+            System.out.println("dubil dit");
+            break;
+        default:
+            System.out.println("mahluk halus");
+            break;
+
+    }
+}
