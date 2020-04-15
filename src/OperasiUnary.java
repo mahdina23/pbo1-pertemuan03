471fda8fc820849e5d745ecb254c049145529e90
muhammadsyarifuddin23 <beldadina987@gmail.com>
2020-04-16 00:47:49 +0800

    OperasiUnary

---
+++ src/OperasiUnary.java
@@ -0,0 +1,11 @@
+public class OperasiUnary {
+    public static void main(String[] args {
+        int i = 0;
+        i++;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        i=i+1;
+        System.out.println(i);
+    }
+}
